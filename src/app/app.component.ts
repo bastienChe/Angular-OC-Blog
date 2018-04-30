e647294b775e51caa78cf386e156eb17be9d9f4a
unknown <bchevriaut@gmail.com>
2018-04-30 19:15:08 +0200

    installation firebase + configuration backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor() {
+   // Initialize Firebase for the whole app
+   const config = {
+      apiKey: 'AIzaSyB5P8Xfe55ELXac6PH_ajiBOe6cC1hdge4',
+      authDomain: 'oc-training-blog.firebaseapp.com',
+      databaseURL: 'https://oc-training-blog.firebaseio.com',
+      projectId: 'oc-training-blog',
+      storageBucket: '',
+      messagingSenderId: '510035295106'
+   };
+   firebase.initializeApp(config);
 
+ }
 
 }
